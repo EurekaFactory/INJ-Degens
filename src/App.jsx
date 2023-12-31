4f783d85d9b94f3a4bef9345d72cd02ec57fcf3f
ChrisRoland <chrisebuberoland@gmail.com>
2023-12-31 12:44:17 +0100

    Webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import TokenComponent from './components/TokenComponent/TokenComponent';
+import { Routes, Route } from 'react-router-dom';
+import WebsiteComponent from './components/WebsiteComponent/websiteComponent';
 
 function App() {
 
   return (
     <>
-      <TokenComponent />
+      <Routes>
+          <Route path="/" element={<WebsiteComponent/>} />
+          <Route path="/project" element={<TokenComponent/>} />
+      </Routes>
+      {/* <WebsiteComponent/>
+      <TokenComponent /> */}
     </>
+    
   )
 }
 
