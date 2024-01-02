e93cbdaf4cda84316db11544230b61553dadb690
ChrisRoland <chrisebuberoland@gmail.com>
2024-01-02 04:19:56 +0100

    fix: UI updates

--- src/components/WebsiteComponent/WebsiteComponent.jsx
+++ src/components/WebsiteComponent/WebsiteComponent.jsx
@@ -7,32 +7,32 @@ const WebsiteComponent = () => {
       <header className="">
 {/* Header Section*/}
         <nav className="w-full flex-row justify-between items-center md:flex">
-          <div className="flex flex-row max-sm:justify-center items-center gap-5 lg:gap-8 xl:gap-10 text-2xl xl:text-3xl">
-            <img src= {logo} alt="logo" className="w-[40px] my-2 rounded-full overflow-hidden shadow-[0_5px_2px_0px_rgba(0,0,0,0.3)] shadow-black"></img>
+          <div className="flex flex-row max-sm:justify-center max-md:justify-center items-center gap-5 lg:gap-8 xl:gap-10 text-2xl xl:text-3xl">
+            <a href="#home"><img src= {logo} alt="logo" className="w-[40px] my-2 rounded-full overflow-hidden shadow-[0_5px_2px_0px_rgba(0,0,0,0.3)] shadow-black"></img></a>
             <p>Injective Degens</p>
           </div>
           
-          <div className="flex flex-row justify-between items-center max-sm:hidden lg:gap-8 xl:gap-10">
-            <a href="#about">About</a>
-            <a href="#disclaimer">Legal Disclaimer</a>
-            <Link to='/project'>Buy Now</Link>
-            <Link to='/airdrop'>Airdrop</Link>
+          <div className="flex flex-row justify-between items-center max-sm:hidden lg:gap-8 md:gap-6 xl:gap-10">
+            <a href="#about" className="underline decoration-yellow-500">About</a>
+            <a href="#disclaimer" className="underline decoration-yellow-500">Legal Disclaimer</a>
+            <Link to='/project' className="underline decoration-yellow-500">Buy Now</Link>
+            <Link to='/airdrop' className="underline decoration-yellow-500">Airdrop</Link>
             <a href="https://x.com/InjectiveDegens" target="_blank" rel="noopener noreferrer"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path></svg></a>
             <a href="https://t.me/injectiveDgnz" target="_blank" rel="noopener noreferrer"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M446.7 98.6l-67.6 318.8c-5.1 22.5-18.4 28.1-37.3 17.5l-103-75.9-49.7 47.8c-5.5 5.5-10.1 10.1-20.7 10.1l7.4-104.9 190.9-172.5c8.3-7.4-1.8-11.5-12.9-4.1L117.8 284 16.2 252.2c-22.1-6.9-22.5-22.1 4.6-32.7L418.2 66.4c18.4-6.9 34.5 4.1 28.5 32.2z"></path></svg></a>
           </div>
         </nav>
       </header>
 
-      <main className="text-2xl max-sm:mx-0 max-sm:text-lg">
+      <main id="home" className="text-2xl max-sm:mx-0 max-sm:text-lg">
 {/* Hero Section */}
         <div className="flex flex-col-reverse lg:flex-row justify-between items-center w-full md:pt-8 lg:pt-12 xl:pt-16">
           <div className="flex flex-col items-center lg:items-start justify-start gap-8 m-[40px_0px] max-sm:m-[20px_0px]">
             <h1 className="font-bold text-7xl max-sm:text-5xl md:text-6xl lg:text-9xl xl:text-8xl">INJECTIVE DEGENS</h1>
             <h3 className=" text-3xl md:text-4xl lg:text-5xl xl:text-5.5xl leading-none text-center md:text-left">DEGENS WILL DEGEN</h3>
             <div className="flex flex-col md:flex-row gap-3 lg:gap-5 xl:gap-7 items-center mt-0 lg:mt-6 xl:mt-8">
-              <a href="https://t.me/injectiveDgnz" target="_blank" rel="noopener noreferrer" className="border-2 bg-yellow-400 hover:bg-yellow-500 text-black border-b w-full shadow-[0_5px_2px_0px_rgba(0,0,0,0.3)] shadow-black text-center text-3xl max-sm:text-2xl xl:text-4xl transition-all duration-500 rounded-xl lg:rounded-2xl py-2 max-sm:px-3 max-sm:py-1 hover:-translate-y-1">TELEGRAM</a>
-              <Link to='/airdrop' rel="noopener noreferrer" className="border-2 bg-yellow-400 hover:bg-yellow-500 text-black shadow-[0_5px_2px_0px_rgba(0,0,0,0.3)] shadow-black text-center text-3xl max-sm:text-2xl xl:text-4xl transition-all duration-500 rounded-xl lg:rounded-2xl py-2 px-6 max-sm:px-3 max-sm:py-1 hover:-translate-y-1">AIRDROP</Link>
-              <Link to='/project' rel="noopener noreferrer" className="border-2 bg-yellow-400 hover:bg-yellow-500 text-black shadow-[0_5px_2px_0px_rgba(0,0,0,0.3)] shadow-black text-center text-3xl max-sm:text-2xl xl:text-4xl transition-all duration-500 rounded-xl lg:rounded-2xl py-2 px-6 max-sm:px-3 max-sm:py-1 hover:-translate-y-1">BUY NOW</Link>
+              <a href="https://t.me/injectiveDgnz" target="_blank" rel="noopener noreferrer" className="border-2 bg-yellow-400 hover:bg-yellow-500 text-black border-b max-sm:w-full shadow-[0_5px_2px_0px_rgba(0,0,0,0.3)] shadow-black text-center text-3xl max-sm:text-2xl xl:text-4xl transition-all duration-500 rounded-xl lg:rounded-2xl py-2 px-3 max-sm:px-3 max-sm:py-1 hover:-translate-y-1">TELEGRAM</a>
+              <Link to='/airdrop' rel="noopener noreferrer" className="border-2 bg-yellow-400 hover:bg-yellow-500 text-black shadow-[0_5px_2px_0px_rgba(0,0,0,0.3)] shadow-black text-center text-3xl max-sm:text-2xl xl:text-4xl transition-all duration-500 rounded-xl lg:rounded-2xl py-2 px-3 max-sm:px-3 max-sm:py-1 hover:-translate-y-1">AIRDROP</Link>
+              <Link to='/project' rel="noopener noreferrer" className="border-2 bg-yellow-400 hover:bg-yellow-500 text-black shadow-[0_5px_2px_0px_rgba(0,0,0,0.3)] shadow-black text-center text-3xl max-sm:text-2xl xl:text-4xl transition-all duration-500 rounded-xl lg:rounded-2xl py-2 px-3 max-sm:px-3 max-sm:py-1 hover:-translate-y-1">BUY NOW</Link>
             </div>
           </div>
           <img src={logo} alt="$DGNZ" className="w-[250px] max-sm:w-[200px] md:w-[325px] lg:w-[375px] xl:w-[300px] 2xl:w-[500px] rounded-full overflow-hidden shadow-[0_5px_2px_0px_rgba(0,0,0,0.3)] shadow-black"></img>
